b34293e3e5671854bbc6e915f84d01a3a986e873
PUERS006 <Sebastian.Puerto.-ND@disney.com>
2024-05-16 11:11:15 -0500

    ✨ Create RDS

--- main.tf
+++ main.tf
@@ -11,8 +11,20 @@ locals {
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 2)
 
+  db_name    = "${terraform.workspace}-postgresql"
+  db_sg_name = "${local.db_name}-sg"
+
+  engine               = "postgres"
+  engine_version       = "14"
+  family               = "postgres14"
+  major_engine_version = "14"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 20
+  port                 = 5432
+  storage_type         = "gp3"
+
   tags = {
-    Terraform   = "true"
+    Terraform = "true"
   }
 }
 
@@ -35,4 +47,62 @@ module "vpc" {
   one_nat_gateway_per_az = false
 
   tags = local.tags
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+
+  name        = local.db_sg_name
+  description = "PostgreSQL security group"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5432
+      to_port     = 5432
+      protocol    = "tcp"
+      description = "PostgreSQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
+
+  tags = local.tags
+}
+
+module "db" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "6.6.0"
+
+  identifier = "${local.db_name}-master"
+
+  engine               = local.engine
+  engine_version       = local.engine_version
+  family               = local.family
+  major_engine_version = local.major_engine_version
+  instance_class       = local.instance_class
+
+  allocated_storage = local.allocated_storage
+
+  db_name  = "movie_db"
+  username = "applicationuser"
+  port     = local.port
+
+  manage_master_user_password = false
+
+  multi_az               = true
+  db_subnet_group_name   = module.vpc.database_subnet_group_name
+  vpc_security_group_ids = [module.security_group.security_group_id]
+
+  maintenance_window              = "Mon:00:00-Mon:03:00"
+  backup_window                   = "03:00-06:00"
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+
+  skip_final_snapshot = true
+  deletion_protection = false
+  storage_encrypted   = false
+
+  tags = local.tags
+
 }
